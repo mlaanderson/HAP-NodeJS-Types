b5743221a6bbc08ee32b460f76247b12fa96c55b
Michael Anderson <manderson@amesbury.com>
2016-07-18 09:25:09 -0500

    Added accessories/types to the export

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = {
   Service: require('./lib/gen/HomeKitTypes').Service,
-  Characteristic: require('./lib/gen/HomeKitTypes').Characteristic
+  Characteristic: require('./lib/gen/HomeKitTypes').Characteristic,
+  TypeUUIDs: require('./accessories/types');
 }
